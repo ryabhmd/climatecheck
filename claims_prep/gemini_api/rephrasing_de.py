8fa8370bf345eb020fdcd15e67fc2047e813c4a0
Raia Abu Ahmad <77779090+ryabhmd@users.noreply.github.com>
2024-10-25 09:07:40 +0200

    Update rephrasing_de.py

--- claims_prep/gemini_api/rephrasing_de.py
+++ claims_prep/gemini_api/rephrasing_de.py
@@ -30,9 +30,33 @@ def main():
     
     for idx, row in klimafakten.iterrows():
         print("Rephrasing Klimafakten...")
-        fact = row['Fact']
+        claim = row['Fact']
+        
         try:
-            response = model.generate_content(f"I will give you a fact about a common misinformation about climate change in German. Your task it to rephrase it as if you were 1. A layperson who believes this fact and is tweeting about it and 2. A layperson who does not believe this fact and is tweeting about it. Give me three tweet options in German for each scenario.  The tweets should be framed as claims and include common linguistic features of tweets such as use of abbreviations and acronyms, use of emojis, use of slang and celloqualisms, flexible use of capitalization, and use of common interjections. Fact: {fact}. Give your answer in a JSON format.")
+            response = model.generate_content(
+               """
+               Aufgabe: Gegeben einer Behauptung aus einem Nachrichtenartikel, formuliere diese so um, als wärst du ein Laie, der darüber tweetet.
+               
+               Einschränkungen:
+               1. Beachte stilistische Eigenschaften von Texten aus sozialen Medien, wie beispielsweise die Nutzung von Slang oder informeller Sprache.
+               2. Gehe nicht zu weit bei deinen Textgenerierungen. Generiere sie so plausibel wie möglich, sodass man denken könnte, ein Mensch hätte sie geschrieben.
+               3. Führe Varianz in die Rhetorik sowie in die syntaktische Struktur deiner Tweets ein. **Nicht jeder Tweet muss eine Frage beinhalten**
+               4. **Generiere Tweets in einem neutralen Ton. Verwende keine Ironie oder Satire.**
+               5. **Bewahre die wissenschaftliche Behauptung, die in der originalen Behauptung steckt.**
+               6. Stelle drei Output-Optionen in einem JSON-Format zur Verfügung, welche eine Liste von Tweets enthält:  {'tweets' : [tweet1, tweet2, tweet3]}.
+               7. Bevor du antwortest, formuliere die Prompt um, erweitere die vorliegende Aufgabe und antworte erst danach.
+               8. Wenn du aufkommende Fragen hast, generiere sie und beantworte sie, bevor du deinen finalen Output generierst.
+               
+               Beispieltweets über ein ähnliches Thema:
+               1. @KonProg 10% von Schleswig-Holstein sind oder waren Moore. Bewirtschaftet ist die Trocken-Emission bei 75 t pro Jahr und Hektar (100x100m).Größtes Problem wäre der Rückkauf, da sich die meisten in Privatbesitz befinden. (Finanzproblem!)
+               2. Wer wissen möchte, wie wir die #Klimakrise ausgelöst und welche politischen und wirtschaftlichen Entscheidungen dazu beigetragen haben, der sollte sich die Doku 'Die Erdzerstörer' ansehen. Vielleicht statt '#Nuhr im Ersten'.
+               3. Wenn man sich zu 7. in ein 30 Quadratmeter Passivhaus mit Holz-Solar Heizung quetscht. Keine Verkehrsmittel nutzt, kein Geld für Konsumgüter ausgibt, und sich Bio-Regional-Saisonal-Vegan ernährt, dann ist man im Budget. Ihr wisst was zu tun ist!
+               4. @EtzWaerggli @karina_vogel_de Koch in der Geruechtekueche Wer so etwas verbreitet traegt massiv dazu bei, dass wir hier im ganz großen Stil getäuscht, belogen, hinter’s Licht geführt werden. Pfui! Wer Greta finanziert ist gleichgueltig, 97% aller Wissenschaftler teilen ihre Besorgnis.
+               5. @BauerWilli_org @NABU_de Lobby d. Agro-Business übt sich in Wagenburg-Mentalität #gruenekreuze: #Insektenschutz, #Dünge-VO, #Klimaschutz – alles wird abgelehnt. Jahrzehntelang wurde verfehlte #Agrarpolitik gegen d. Widerstand d. Gesellschaft forciert u. nun wundert man sich, dass man alleine da steht.
+               
+               Behauptung: """+ claim + """
+               
+               Output: """)
             gemini_rephrasings.append(response.text)
         
         except Exception as e:
@@ -54,7 +78,30 @@ def main():
         claim = row['Behauptung']
         
         try:
-            response = model.generate_content(f"I will give you a claim about climate change in German. Your task it to rephrase it as if you were a layperson who believes this fact and is tweeting about it. Give me three tweet options in German for each scenario. The tweets should be framed as claims and include common linguistic features of tweets such as use of abbreviations and acronyms, use of emojis, use of slang and celloqualisms, flexible use of capitalization, and use of common interjections. Claim: {claim}. Give your answer in a JSON format.")
+            response = model.generate_content(
+               """
+               Aufgabe: Gegeben einer Behauptung aus einem Nachrichtenartikel, formuliere diese so um, als wärst du ein Laie, der darüber tweetet.
+               
+               Einschränkungen:
+               1. Beachte stilistische Eigenschaften von Texten aus sozialen Medien, wie beispielsweise die Nutzung von Slang oder informeller Sprache.
+               2. Gehe nicht zu weit bei deinen Textgenerierungen. Generiere sie so plausibel wie möglich, sodass man denken könnte, ein Mensch hätte sie geschrieben.
+               3. Führe Varianz in die Rhetorik sowie in die syntaktische Struktur deiner Tweets ein. **Nicht jeder Tweet muss eine Frage beinhalten**
+               4. **Generiere Tweets in einem neutralen Ton. Verwende keine Ironie oder Satire.**
+               5. **Bewahre die wissenschaftliche Behauptung, die in der originalen Behauptung steckt.**
+               6. Stelle drei Output-Optionen in einem JSON-Format zur Verfügung, welche eine Liste von Tweets enthält:  {'tweets' : [tweet1, tweet2, tweet3]}.
+               7. Bevor du antwortest, formuliere die Prompt um, erweitere die vorliegende Aufgabe und antworte erst danach.
+               8. Wenn du aufkommende Fragen hast, generiere sie und beantworte sie, bevor du deinen finalen Output generierst.
+               
+               Beispieltweets über ein ähnliches Thema:
+               1. @KonProg 10% von Schleswig-Holstein sind oder waren Moore. Bewirtschaftet ist die Trocken-Emission bei 75 t pro Jahr und Hektar (100x100m).Größtes Problem wäre der Rückkauf, da sich die meisten in Privatbesitz befinden. (Finanzproblem!)
+               2. Wer wissen möchte, wie wir die #Klimakrise ausgelöst und welche politischen und wirtschaftlichen Entscheidungen dazu beigetragen haben, der sollte sich die Doku 'Die Erdzerstörer' ansehen. Vielleicht statt '#Nuhr im Ersten'.
+               3. Wenn man sich zu 7. in ein 30 Quadratmeter Passivhaus mit Holz-Solar Heizung quetscht. Keine Verkehrsmittel nutzt, kein Geld für Konsumgüter ausgibt, und sich Bio-Regional-Saisonal-Vegan ernährt, dann ist man im Budget. Ihr wisst was zu tun ist!
+               4. @EtzWaerggli @karina_vogel_de Koch in der Geruechtekueche Wer so etwas verbreitet traegt massiv dazu bei, dass wir hier im ganz großen Stil getäuscht, belogen, hinter’s Licht geführt werden. Pfui! Wer Greta finanziert ist gleichgueltig, 97% aller Wissenschaftler teilen ihre Besorgnis.
+               5. @BauerWilli_org @NABU_de Lobby d. Agro-Business übt sich in Wagenburg-Mentalität #gruenekreuze: #Insektenschutz, #Dünge-VO, #Klimaschutz – alles wird abgelehnt. Jahrzehntelang wurde verfehlte #Agrarpolitik gegen d. Widerstand d. Gesellschaft forciert u. nun wundert man sich, dass man alleine da steht.
+               
+               Behauptung: """+ claim + """
+               
+               Output: """)
             gemini_rephrasings.append(response.text)
             
         except Exception as e:
