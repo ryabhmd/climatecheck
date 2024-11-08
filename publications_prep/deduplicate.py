2223265557c587a13d2751b8ff197760d4ee57dd
Raia Abu Ahmad <77779090+ryabhmd@users.noreply.github.com>
2024-11-08 15:00:53 +0100

    Update deduplicate.py

--- publications_prep/deduplicate.py
+++ publications_prep/deduplicate.py
@@ -14,10 +14,10 @@ def main():
 	print(f'Number of S2ORC publications: {len(s2orc_publications)}.')
 	print(f'Number of OpenAlex publications: {len(open_alex_publlications)}.')
 
-	s2orc_publications_deduped = s2orc_publications[~s2orc_publications['doi'].isin(open_alex_publlications['doi'])]
-	print(f'Number of S2ORC publications after deduplication: {len(s2orc_publications_deduped)}.')
+	#s2orc_publications_deduped = s2orc_publications[~s2orc_publications['doi'].isin(open_alex_publlications['doi'])]
+	#print(f'Number of S2ORC publications after deduplication: {len(s2orc_publications_deduped)}.')
 
-	s2orc_publications_deduped.to_pickle('/netscratch/abu/Shared-Tasks/ClimateCheck/data/publications/s2orc_publications_deduplicated.pkl')
+	#s2orc_publications_deduped.to_pickle('/netscratch/abu/Shared-Tasks/ClimateCheck/data/publications/s2orc_publications_deduplicated.pkl')
 
 
 if __name__ == "__main__":
