3a58f2d877152c59a5d36d5d4dcbe47e19595eaa
Raia Abu Ahmad <77779090+ryabhmd@users.noreply.github.com>
2024-11-08 15:00:10 +0100

    Update deduplicate.py

--- publications_prep/deduplicate.py
+++ publications_prep/deduplicate.py
@@ -9,7 +9,8 @@ def main():
 	s2orc_publications['doi'] = 'https://doi.org/' + s2orc_publications['doi']
 
 	open_alex_publlications = pd.read_pickle('/netscratch/abu/Shared-Tasks/ClimateCheck/data/publications/combined_climate_works_filtered.pkl')
-
+	print(open_alex_publlications.columns)
+	
 	print(f'Number of S2ORC publications: {len(s2orc_publications)}.')
 	print(f'Number of OpenAlex publications: {len(open_alex_publlications)}.')
 
